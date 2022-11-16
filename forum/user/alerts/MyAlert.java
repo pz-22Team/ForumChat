92c863eb277461f88f1b7d37a61107cc45ac596b
rrromchIk <90086332+rrromchIk@users.noreply.github.com>
2022-11-16 13:02:10 +0200

    Update MyAlert.java

--- forum/user/alerts/MyAlert.java
+++ forum/user/alerts/MyAlert.java
@@ -1,26 +1,29 @@
 package com.forum.user.alerts;
 
+import com.forum.user.app.UserApplication;
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 public class MyAlert extends Alert {
-
     public static final AlertType WARNING = AlertType.WARNING;
     public static final AlertType ERROR = AlertType.ERROR;
     public static final AlertType INFORMATION = AlertType.INFORMATION;
 
-    public MyAlert(AlertType alertType, String title) {
-        super(alertType);
+    public MyAlert(String title) {
+        super(AlertType.INFORMATION);
         super.setTitle(title);
     }
 
-    public void show(String headerText, String bodyText) {
+    public void show(AlertType type, String headerText, String bodyText) {
+        super.setAlertType(type);
         super.setHeaderText(headerText);
         super.setContentText(bodyText);
-        /*((Stage)super.getDialogPane()
+        ((Stage)super.getDialogPane()
                 .getScene()
                 .getWindow())
                 .getIcons()
-                .add(new Image(""));*/
+                .add(new Image(UserApplication.ICON_PATH));
         super.showAndWait();
     }
 }
