1c474fa668616d31c9910d991753d34ce12b96df
rrromchIk <90086332+rrromchIk@users.noreply.github.com>
2022-11-22 00:16:42 +0200

    Delete forum-ui directory

--- forum-ui/myexception.cpp
+++
@@ -1,7 +0,0 @@
-#include "myexception.h"
-
-MyException::MyException(std::string text ):exceptionText(text)  {}
-
-const char *MyException::what() const noexcept {
-    return exceptionText.c_str();
-}
